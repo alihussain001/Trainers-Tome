9fa618389e7c6b0a8bedfcf84b6bf1b31cb8fe54
alihussain <ahalihussain11@gmail.com>
2025-01-08 21:51:17 +0500

    created a structure for code

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("hello world");
+    res.send("your application is running");
 } );
 
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:${PORT}')
-})
+});
