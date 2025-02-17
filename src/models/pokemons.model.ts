8ab4c152f6c0a4f2b90eaa194046da291baf7a50
alihussain <ahalihussain11@gmail.com>
2025-02-18 02:52:34 +0500

    Data model for pokemon created

---
+++ src/models/pokemons.model.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+export interface IPokemon{
+    name: string;
+    type: string;
+    level: number;
+    abilities: string[];
+    stats: {
+        hp: number;
+        attack: number;
+        defense: number;
+        speed: number;
+    };
+}
+
+const PokemonSchema = new Schema<IPokemon>({
+    name: { type: String, required: true, unique: true},
+    type: { type: String, required: true},
+    level: { type: Number, required: true, default: 1},
+    abilities: { type: [String], required:true},
+    stats: {
+        hp: { type: Number, required: true},
+        attack: { type: Number, required: true},
+        defense: { type: Number, required: true},
+        speed: { type: Number, required: true}
+    },
+
+});
+
+const Pokemon = model<IPokemon>('Pokemon', PokemonSchema);
+export default Pokemon;
